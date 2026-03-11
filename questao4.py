a26c65fb363a425ba63007a5581ee80ff4b3f11d
ryanssilva7-stack <ryan.s.silva7@ba.estudante.senai.br>
2026-03-11 15:07:41 -0300

    questao4.py

--- questao4.py
+++ questao4.py
@@ -2,4 +2,28 @@ import os
 os.system("cls")
 
 # dados
-fruta = input("Escolha uma fruta entre maçã e morango: ").lower()
+fruta = input("Escolha uma fruta: ").lower()
+kg = int(input("Indique quantos KG deseja: "))
+
+# processo
+
+if fruta == "morango" and kg <= 5:
+    preco = 2.50
+else:
+    preco = 2.20
+    
+if fruta == "maçã" and kg <= 5:
+    preco = 1.80
+else:
+    preco = 1.50
+
+if preco > 15 or kg <= 10:
+    print("\nDesconto de 10%")
+    valor = (preco + kg) * 0.1
+else:
+    valor = preco + kg
+
+arrendondar = round (valor, 2)
+
+# saida
+print(f"Valor á pagar: R${valor}")
